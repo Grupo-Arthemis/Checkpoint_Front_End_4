ff0365fec43ed0937979c19b3b33f002c43b0984
luanmacea <luanmacea@gmail.com>
2023-08-30 19:38:54 -0300

    Catalogo funcionando

--- script/catalogo.js
+++ script/catalogo.js
@@ -1,3 +1,6 @@
+// Limpar local storage quando ini9ciar a pagina (tem que fazer funcao para ver se esta cadastrado ou se ja tinha coisa)
+localStorage.clear();
+
 let vinhos; 
 
 function carregarDadosVinhos() {
@@ -128,60 +131,142 @@ function addProdGale(nome,preco,ano,auto){
       }
 };
 
-function addAoCarrinho(botao) {
 
-    let sacola = document.querySelector('#quantItensSacola');
-    let div = botao.parentNode;
-    if (div.parentElement.parentElement.className == "destaque-card svg-container"){
-      console.log(div.parentElement.querySelector(".nomeVinhoDestaque").innerHTML);
-      console.log(div.parentElement.querySelector(".precoVinhoDestaque").innerHTML);
-      console.log(div.parentElement.querySelector(".anoVinhoDestaque").innerHTML);
-    }else{
-      console.log(div.parentElement.parentElement.querySelector(".precoVinhoDestaque").innerHTML);
-      console.log(div.parentElement.parentElement.querySelector(".anoVinhoDestaque").innerHTML);
-      console.log(div.parentElement.parentElement.querySelector(".nomeVinhoDestaque").innerHTML);
+function addAoCarrinho(botao){
+  let sacola = document.querySelector('#quantItensSacola');
+  let div = botao.parentNode;
+  div.innerHTML = "<p class='subtrair'>-</p>" +
+                  "<div class='circulop'><p class='addCarrinho'>0</p></div>" +
+                  "<p class='somar'>+</p>";
+  ;
+  
+  // pegando as informacoes do vinho
+  const cardInfoDiv = div.previousElementSibling;
+  const nomeVinho = cardInfoDiv.querySelector('.nomeVinhoDestaque, .nomeVinhoDestaque-grid').textContent;
+  const precoVinho = cardInfoDiv.querySelector('.precoVinhoDestaque, .precoVinhoDestaque-grid').textContent;
+  
+  const vinho = {
+    nome: nomeVinho,
+    preco: precoVinho,
+    quantidade: 1
+  };
+  
+  //adicionando local storage
+  var listaVinhos = JSON.parse(localStorage.getItem("listaVinhos")) || [];
+  
+  
+  let sum = div.querySelector('.somar');
+  sum.addEventListener('click', function() {
+    const quantidadeSac = div.querySelector('.addCarrinho');
+    const vinhoExistente = listaVinhos.find(item => item.nome === nomeVinho);
+    //verifica se tem no local storage
+    
+    if (vinhoExistente) {
+      vinhoExistente.quantidade++;
+      quantidadeSac.textContent = vinhoExistente.quantidade;
+    } else {
+      listaVinhos.push(vinho); 
+      quantidadeSac.textContent = 1;
     }
-      div.innerHTML = "<p class='subtrair'>-</p>" +
-                      "<div class='circulop'><p class='addCarrinho'>1</p></div>" +
-                      "<p class='somar'>+</p>";
+    
+    localStorage.setItem('listaVinhos', JSON.stringify(listaVinhos));
+  
+    //sacola ter a quantidade de itens pedidas
+    const totalQuantidade = listaVinhos.reduce((total, item) => total + item.quantidade, 0);
+    sacola.innerHTML = totalQuantidade;
+    
+    console.log(`Vinho Adicionado! Nome: ${nomeVinho}`);
+    console.log(`Vinho Adicionado! Nome: ${precoVinho}`);
+    console.log(listaVinhos) 
+  });
+  //Logica para subtrair
+  let sub = div.querySelector('.subtrair');
+  sub.addEventListener('click', function() {
+    const quantidadeSac = div.querySelector('.addCarrinho');
+    const vinhoExistente = listaVinhos.find(item => item.nome === nomeVinho);
 
-      sacola.innerHTML = parseInt(sacola.innerHTML) + 1;
-
-      let sum = div.querySelector('.somar');
-
-      sum.addEventListener('click', () => {
-        const quantidade = div.querySelector('.addCarrinho');
-        if (quantidade.innerHTML <=0){
-          div.innerHTML =
+    if (vinhoExistente && vinhoExistente.quantidade > 0) {
+      vinhoExistente.quantidade--;
+      quantidadeSac.textContent = vinhoExistente.quantidade;
+    }
+    if (vinhoExistente && vinhoExistente.quantidade == 0){
+      div.innerHTML =
             "<div class='circulo'>" +
             "<img src='../assets/carrinho.svg' alt='' class='addCarrinho'> " +
             "</div>";
-            sacola.innerHTML = 0
+      ;
+      sacola.innerHTML = 0
 
-            let newCirculo = div.querySelector('.circulo');
-            newCirculo.addEventListener('click', () => {
-            (addAoCarrinho(newCirculo))})
-        }else{
-        quantidade.innerHTML = parseInt(quantidade.innerHTML) + 1
-        sacola.innerHTML = parseInt(sacola.innerHTML) + 1}});
+      let newCirculo = div.querySelector('.circulo');
+      newCirculo.addEventListener('click', () => {
+      (addAoCarrinho(newCirculo))})
+
+    }
+    localStorage.setItem('listaVinhos', JSON.stringify(listaVinhos));
+
+    const totalQuantidade = listaVinhos.reduce((total, item) => total + item.quantidade, 0);
+    sacola.innerHTML = totalQuantidade;
+
+    console.log(`Vinho Removido! Nome: ${nomeVinho}`);
+    console.log(`Vinho Removido! Nome: ${precoVinho}`);
+    console.log(listaVinhos) 
+  });
+}
+
+// function addAoCarrinho(botao) {
+
+//     let sacola = document.querySelector('#quantItensSacola');
+//     let div = botao.parentNode;
+//     if (div.parentElement.parentElement.className == "destaque-card svg-container"){
+//       console.log(div.parentElement.querySelector(".nomeVinhoDestaque").innerHTML);
+//       console.log(div.parentElement.querySelector(".precoVinhoDestaque").innerHTML);
+//       console.log(div.parentElement.querySelector(".anoVinhoDestaque").innerHTML);
+//     }else{
+//       console.log(div.parentElement.parentElement.querySelector(".precoVinhoDestaque").innerHTML);
+//       console.log(div.parentElement.parentElement.querySelector(".anoVinhoDestaque").innerHTML);
+//       console.log(div.parentElement.parentElement.querySelector(".nomeVinhoDestaque").innerHTML);
+//     }
+//       div.innerHTML = "<p class='subtrair'>-</p>" +
+//                       "<div class='circulop'><p class='addCarrinho'>1</p></div>" +
+//                       "<p class='somar'>+</p>";
+
+//       sacola.innerHTML = parseInt(sacola.innerHTML) + 1;
+
+//       let sum = div.querySelector('.somar');
+
+//       sum.addEventListener('click', () => {
+//         const quantidade = div.querySelector('.addCarrinho');
+//         if (quantidade.innerHTML <=0){
+//           div.innerHTML =
+//             "<div class='circulo'>" +
+//             "<img src='../assets/carrinho.svg' alt='' class='addCarrinho'> " +
+//             "</div>";
+//             sacola.innerHTML = 0
+
+//             let newCirculo = div.querySelector('.circulo');
+//             newCirculo.addEventListener('click', () => {
+//             (addAoCarrinho(newCirculo))})
+//         }else{
+//         quantidade.innerHTML = parseInt(quantidade.innerHTML) + 1
+//         sacola.innerHTML = parseInt(sacola.innerHTML) + 1}});
         
-        let sub = div.querySelector('.subtrair');
-        sub.addEventListener('click', () => {
-          const quantidade = div.querySelector('.addCarrinho');
-          quantidade.innerHTML = parseInt(quantidade.innerHTML) - 1
-          sacola.innerHTML = parseInt(sacola.innerHTML) - 1
-          div
-          if (quantidade.innerHTML <= 0){
-            div.innerHTML =
-            "<div class='circulo'>" +
-            "<img src='../assets/carrinho.svg' alt='' class='addCarrinho'> " +
-            "</div>";
-            sacola.innerHTML = 0
+//         let sub = div.querySelector('.subtrair');
+//         sub.addEventListener('click', () => {
+//           const quantidade = div.querySelector('.addCarrinho');
+//           quantidade.innerHTML = parseInt(quantidade.innerHTML) - 1
+//           sacola.innerHTML = parseInt(sacola.innerHTML) - 1
+//           div
+//           if (quantidade.innerHTML <= 0){
+//             div.innerHTML =
+//             "<div class='circulo'>" +
+//             "<img src='../assets/carrinho.svg' alt='' class='addCarrinho'> " +
+//             "</div>";
+//             sacola.innerHTML = 0
 
-            let newCirculo = div.querySelector('.circulo');
-            newCirculo.addEventListener('click', () => {
-            (addAoCarrinho(newCirculo))})}})
-};
+//             let newCirculo = div.querySelector('.circulo');
+//             newCirculo.addEventListener('click', () => {
+//             (addAoCarrinho(newCirculo))})}})
+// };
 
 alterarCoresBloob("randon","randon");
 
