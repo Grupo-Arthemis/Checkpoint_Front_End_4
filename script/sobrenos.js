ce9f65271d91617b1e6a6968355bb89173891d84
luanmacea <luanmacea@gmail.com>
2023-08-31 19:41:36 -0300

    peguenos erros no formulario

--- script/sobrenos.js
+++ script/sobrenos.js
@@ -1,3 +1,5 @@
+var emailRegExp = /\S+@\S+\.\S{3,}/;
+
 var map = L.map('map').setView([-23.503370, -47.147703], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -19,7 +21,10 @@ botao.addEventListener('click', function() {
         event.preventDefault()
         alert("Preencha todos os campos!");
         return false;
-    } else {
+    } else if (!emailRegExp.test(email.value)){
+        event.preventDefault()
+        alert("Por favor, informe um e-mail válido.")
+    }else{
         alert("Mensagem enviada com sucesso!");
     }
 });
